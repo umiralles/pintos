2c355c88da6afeef5cf12b0c4801b5ba9583d4a7
Mark Laptop <mark.wheelhouse@imperial.ac.uk>
2020-11-02 16:14:42 +0000

    updated ELF executable loader to handle overlapping program segments (resulting from modern gcc toolchains)

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -395,26 +395,34 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
          and zero the final PAGE_ZERO_BYTES bytes. */
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
+      
+      /* Check if virtual page already allocated */
+      struct thread *t = thread_current ();
+      uint8_t *kpage = pagedir_get_page (t->pagedir, upage);
+      
+      if (kpage == NULL){
+        
+        /* Get a new page of memory. */
+        kpage = palloc_get_page (PAL_USER);
+        if (kpage == NULL){
+          return false;
+        }
+        
+        /* Add the page to the process's address space. */
+        if (!install_page (upage, kpage, writable)) 
+        {
+          palloc_free_page (kpage);
+          return false; 
+        }        
+      }
 
-      /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
-      if (kpage == NULL)
-        return false;
-
-      /* Load this page. */
+      /* Load data into the page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
           palloc_free_page (kpage);
           return false; 
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
-
-      /* Add the page to the process's address space. */
-      if (!install_page (upage, kpage, writable)) 
-        {
-          palloc_free_page (kpage);
-          return false; 
-        }
 
       /* Advance. */
       read_bytes -= page_read_bytes;
