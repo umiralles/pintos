771d17fb0fa1dbe784f146dc79b877769f00f8ff
Nicole <no119@ic.ac.uk>
2020-11-07 15:15:46 +0000

    Cleaned up code for syscalls to improve readability and maintain consistent style [NO]

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,108 +1,63 @@
-#include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 #include "filesys/off_t.h"
 #include "filesys/file.h"
-#include "pagedir.h"
-#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
+#include "userprog/syscall.h"
 
 static void syscall_handler (struct intr_frame *);
-static void syscall_acquire_lock(struct lock *);
 
 /* SYSTEM CALL FUNCTIONS */
-static void halt(struct intr_frame *f UNUSED) {}
-static void exit(struct intr_frame *f);
-static void exec(struct intr_frame *f UNUSED) {}
-static void wait(struct intr_frame *f UNUSED) {}
-static void create(struct intr_frame *f UNUSED) {}
-static void remove(struct intr_frame *f UNUSED) {}
-static void open(struct intr_frame *f UNUSED) {}
-static void filesize(struct intr_frame *f UNUSED){}
-static void read(struct intr_frame *f UNUSED) {}
-static void write(struct intr_frame *f);
-static void seek(struct intr_frame *f UNUSED) {}
-static void tell(struct intr_frame *f UNUSED) {}
-static void close(struct intr_frame *f UNUSED) {}
+static void syscall_halt(struct intr_frame *f UNUSED) {}
+static void syscall_exit(struct intr_frame *f);
+static void syscall_exec(struct intr_frame *f UNUSED) {}
+static void syscall_wait(struct intr_frame *f UNUSED) {}
+static void syscall_create(struct intr_frame *f UNUSED) {}
+static void syscall_remove(struct intr_frame *f UNUSED) {}
+static void syscall_open(struct intr_frame *f UNUSED) {}
+static void syscall_filesize(struct intr_frame *f UNUSED) {}
+static void syscall_read(struct intr_frame *f UNUSED) {}
+static void syscall_write(struct intr_frame *f);
+static void syscall_seek(struct intr_frame *f UNUSED) {}
+static void syscall_tell(struct intr_frame *f UNUSED) {}
+static void syscall_close(struct intr_frame *f UNUSED) {}
 
 /* MEMORY ACCESS FUNCTION */
 static void syscall_access_memory(const void *vaddr);
 
-static syscall_func syscalls[MAX_SYSCALLS] = {&halt, &exit, &exec, &wait,
-					       &create, &remove, &open,
-					       &filesize, &read, &write, &seek,
-					       &tell, &close};
+/* HELPER FUNCTIONS */
+static void *get_argument(void *esp, int arg_no);
+static void return_value_to_frame(struct intr_frame *f, uint32_t val);
+static void syscall_acquire_lock(struct lock *);
 
-void
-syscall_init (void) 
-{
+/* Jump table used to call a syscall */
+static syscall_func syscalls[MAX_SYSCALLS] = {&syscall_halt, &syscall_exit,
+					      &syscall_exec, &syscall_wait,
+					      &syscall_create, &syscall_remove,
+					      &syscall_open, &syscall_filesize,
+					      &syscall_read, &syscall_write,
+					      &syscall_seek, &syscall_tell,
+					      &syscall_close};
+
+void syscall_init(void) {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void *get_argument(void *esp, int arg_no) {
-  syscall_access_memory(esp + arg_no * 4);
-  return  esp + arg_no * 4;
-}
-
-static void return_value_to_frame(struct intr_frame *f, uint32_t val) {
-  syscall_access_memory(&f->eax);
-  f->eax = val;
-}
-
-static void
-syscall_handler (struct intr_frame *f) 
-{
+static void syscall_handler(struct intr_frame *f) {
   syscall_access_memory(f->esp);
   int32_t call_no = *((int32_t *) f->esp);
   syscalls[call_no](f);
 }
 
-static void exit(struct intr_frame *f) {
+static void syscall_exit(struct intr_frame *f) {
   int status = *((int *) get_argument(f->esp, 1));
   return_value_to_frame(f, (uint32_t) status);
   thread_exit();
 }
 
-/* One lock in a list. */
-struct lock_elem {
-  struct list_elem elem;              /* List element. */
-  struct lock *lock;         	      /* This lock. */
-};
-
-// needs to be thread-safe!
-// not sure if this is needed, but we need some way of finding which locks are held by the thread
-static void
-syscall_acquire_lock(struct lock *lock) {
-  // acquire the lock and add it to a list
-  lock_acquire(lock);
-  struct lock_elem lock_elem;
-  lock_elem.lock = lock;
-  list_push_back(&thread_current()->held_locks, &lock_elem.elem);
-}
-
-static void
-syscall_access_memory(const void *vaddr) {
-  struct thread *t = thread_current();
-  if(!(is_user_vaddr(vaddr) && pagedir_get_page(t->pagedir, vaddr))) {
-    pagedir_destroy(t->pagedir);
-    struct list_elem *e;
-    for (e = list_begin (&t->held_locks);
-		    e != list_end (&t->held_locks);
-		    e = list_next (e)) {
-      struct lock_elem *elem = list_entry(e, struct lock_elem, elem);
-      lock_release(elem->lock);
-    }
-    thread_exit();
-  }
-}
-
-struct file_elem {
-  int fd;
-  struct file *file;
-  struct list_elem elem;
-};
-
-static void write(struct intr_frame *f){
+static void syscall_write(struct intr_frame *f) {
   int fd = GET_ARGUMENT_VALUE(f, int, 1);
   const void *buffer = GET_ARGUMENT_VALUE(f, void *, 2);
   unsigned size = GET_ARGUMENT_VALUE(f, unsigned, 3);
@@ -122,3 +77,44 @@ static void write(struct intr_frame *f){
   }
   f->eax = bytes_written;
 }
+
+/* MEMORY ACCESS FUNCTION */
+
+static void syscall_access_memory(const void *vaddr) {
+  struct thread *t = thread_current();
+  if(!(is_user_vaddr(vaddr) && pagedir_get_page(t->pagedir, vaddr))) {
+    pagedir_destroy(t->pagedir);
+    struct list_elem *e;
+    for(e = list_begin (&t->held_locks);
+	e != list_end (&t->held_locks);
+	e = list_next (e)) {
+      struct lock_elem *elem = list_entry(e, struct lock_elem, elem);
+      lock_release(elem->lock);
+    }
+    thread_exit();
+  }
+}
+
+/* HELPER FUNCTIONS */
+
+static void *get_argument(void *esp, int arg_no) {
+  syscall_access_memory(esp + arg_no * 4);
+  return  esp + arg_no * 4;
+}
+
+static void return_value_to_frame(struct intr_frame *f, uint32_t val) {
+  syscall_access_memory(&f->eax);
+  f->eax = val;
+}
+
+/* Needs to be thread-safe!
+   Not sure if this is needed, but we need some way of finding
+   which locks are held by the thread */
+static void syscall_acquire_lock(struct lock *lock) {
+  // acquire the lock and add it to a list
+  lock_acquire(lock);
+  struct lock_elem lock_elem;
+  lock_elem.lock = lock;
+  list_push_back(&thread_current()->held_locks, &lock_elem.elem);
+}
+
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,17 +3,35 @@
 
 #include "threads/interrupt.h"
 
-/* the number of implemented and working system calls in the syscall table */
+/* The number of implemented and working system calls in the syscall table */
 #define MAX_SYSCALLS (13)
 
 /* Takes the value of the argument pointer provided by get_argument */
 #define GET_ARGUMENT_VALUE(frame, type, no)	\
   *((type *) get_argument(frame->esp, no))
 
-/* function template for a syscall operation 
-   arguments and return location found in f
-*/
+/* Function template for a syscall operation 
+   arguments and return location found in f */
 typedef void (*syscall_func)(struct intr_frame *f);
+
+/* !!! CURRENTLY both structs in the .h file are there 
+   to help with readability - can be moved in the .c 
+   upon further consideration !!!*/
+
+/* File element struct */
+struct file_elem {
+  int fd;
+  struct file *file;
+  struct list_elem elem;
+};
+
+/* One lock in a list. */
+
+struct lock_elem {
+  struct list_elem elem;              /* List element. */
+  struct lock *lock;         	      /* This lock. */
+};
+
 void syscall_init (void);
 
 #endif /* userprog/syscall.h */
