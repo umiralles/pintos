386575904853edf0ac3a94416044bc21c1330653
Matthew Hill <mh1619@ic.ac.uk>
2020-11-07 13:18:39 +0000

    debugged system call handlers to compile and ran basic programs to test set up steps [MH, UM, IK, NO]

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,28 +4,29 @@
 #include "threads/thread.h"
 #include "filesys/off_t.h"
 #include "filesys/file.h"
+#include "pagedir.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
-static void syscall_acquire_lock(struct lock);
-static void syscall_access_memory(const void *);
+static void syscall_acquire_lock(struct lock *);
 
 /* SYSTEM CALL FUNCTIONS */
-static void halt(struct intr_frame *f) {}
+static void halt(struct intr_frame *f UNUSED) {}
 static void exit(struct intr_frame *f);
-static void exec(struct intr_frame *f) {}
-static void wait(struct intr_frame *f) {}
-static void create(struct intr_frame *f) {}
-static void remove(struct intr_frame *f) {}
-static void open(struct intr_frame *f) {}
-static void filesize(struct intr_frame *f){}
-static void read(struct intr_frame *f) {}
+static void exec(struct intr_frame *f UNUSED) {}
+static void wait(struct intr_frame *f UNUSED) {}
+static void create(struct intr_frame *f UNUSED) {}
+static void remove(struct intr_frame *f UNUSED) {}
+static void open(struct intr_frame *f UNUSED) {}
+static void filesize(struct intr_frame *f UNUSED){}
+static void read(struct intr_frame *f UNUSED) {}
 static void write(struct intr_frame *f);
-static void seek(struct intr_frame *f) {}
-static void tell(struct intr_frame *f) {}
-static void close(struct intr_frame *f) {}
+static void seek(struct intr_frame *f UNUSED) {}
+static void tell(struct intr_frame *f UNUSED) {}
+static void close(struct intr_frame *f UNUSED) {}
 
 /* MEMORY ACCESS FUNCTION */
-static void syscall_access_memory(const void *f) {}
+static void syscall_access_memory(const void *vaddr);
 
 static syscall_func syscalls[MAX_SYSCALLS] = {&halt, &exit, &exec, &wait,
 					       &create, &remove, &open,
@@ -39,20 +40,26 @@ syscall_init (void)
 }
 
 static void *get_argument(void *esp, int arg_no) {
+  syscall_access_memory(esp + arg_no * 4);
   return  esp + arg_no * 4;
+}
+
+static void return_value_to_frame(struct intr_frame *f, uint32_t val) {
+  syscall_access_memory(&f->eax);
+  f->eax = val;
 }
 
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  //check user mem access
+  syscall_access_memory(f->esp);
   int32_t call_no = *((int32_t *) f->esp);
   syscalls[call_no](f);
 }
 
 static void exit(struct intr_frame *f) {
-  int status = (int) get_argument(f->esp, 1);
-  f->eax = (uint32_t) status;
+  int status = *((int *) get_argument(f->esp, 1));
+  return_value_to_frame(f, (uint32_t) status);
   thread_exit();
 }
 
@@ -66,7 +73,7 @@ struct lock_elem {
 static void
 syscall_acquire_lock(struct lock *lock) {
   // acquire the l;ock somehow and add it to some list
-  acquire_lock(lock);
+  lock_acquire(lock);
   struct lock_elem lock_elem;
   lock_elem.lock = lock;
   list_push_back(&thread_current()->held_locks, &lock_elem.elem);
@@ -74,11 +81,12 @@ syscall_acquire_lock(struct lock *lock) {
 
 static void
 syscall_access_memory(const void *vaddr) {
-  if(!(is_user_vaddr(vaddr) && pagedir_get_page(vaddr))) {
-    pagedir_destroy(thread_current()->pagedir);
+  struct thread *t = thread_current();
+  if(!(is_user_vaddr(vaddr) && pagedir_get_page(t->pagedir, vaddr))) {
+    pagedir_destroy(t->pagedir);
     struct list_elem *e;
-    for (e = list_begin (&thread_current()->held_locks);
-		    e != list_end (&thread_current()->held_locks);
+    for (e = list_begin (&t->held_locks);
+		    e != list_end (&t->held_locks);
 		    e = list_next (e)) {
       struct lock_elem *elem = list_entry(e, struct lock_elem, elem);
       lock_release(elem->lock);
@@ -94,11 +102,11 @@ struct file_elem {
 };
 
 static void write(struct intr_frame *f){
-  int fd = (int) get_argument(f->esp, 1);
-  const void *buffer = get_argument(f->esp, 2);
-  unsigned size = (unsigned) get_argument(f->esp, 3);
+  int fd = *((int *) get_argument(f->esp, 1));
+  const void *buffer = *((void **) get_argument(f->esp, 2));
+  unsigned size = *((unsigned *) get_argument(f->esp, 3));
   off_t bytes_written;
-  if(fd == 1) {
+  if(fd == STDOUT_FILENO) {
     putbuf(buffer, size);
     bytes_written = (off_t) size;
   } else {
