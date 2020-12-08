4daff94fc5ca8e130865b5c3e6612f95e1f5b647
Matthew Hill <mh1619@ic.ac.uk>
2020-12-08 11:18:31 +0000

    Implemented a framework for the shared pages to be stored in an extra table [IK, MH]

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -9,14 +9,28 @@
 static struct hash frame_table;
 static struct lock frame_table_lock;
 
+/* Shared table */
+static struct hash shared_table;
+static struct lock shared_table_lock;
+
 /* Hash functions for frame_table */
 static hash_hash_func hash_user_address;
 static hash_less_func cmp_user_address;
+
+/* Hash functions for shared_table */
+static hash_hash_func hash_file;
+static hash_less_func cmp_file;
 
 /* Initialise frame_table and frame_table_lock */
 void ft_init(void) {
   hash_init (&frame_table, hash_user_address, cmp_user_address, NULL);
   lock_init(&frame_table_lock);
+}
+
+/* Initialise shared table */
+void st_init(void) {
+  hash_init(&shared_table, hash_file, cmp_file, NULL);
+  lock_init(&shared_table_lock);
 }
 
 /* Generates a hash function from the user virtual address of a page 
@@ -27,7 +41,7 @@ static unsigned hash_user_address(const struct hash_elem *e, void *aux UNUSED) {
   return ((unsigned) ft->uaddr) / PGSIZE;
 }
 
-/* Compares two frame table elems based on timestamp they were added in */
+/* Compares two frame table elems based on user address */
 static bool cmp_user_address(const struct hash_elem *a,
 			     const struct hash_elem *b, void *aux UNUSED) {
   struct frame_table_entry *ft1 = hash_entry(a, struct frame_table_entry, elem);
@@ -36,9 +50,35 @@ static bool cmp_user_address(const struct hash_elem *a,
   return ft1->uaddr < ft2->uaddr;
 }
 
-/* Inserts hash_elem elem into the frame_table */
+/* Generates a hash function from the file pointer address of a shared page */
+static unsigned hash_file(const struct hash_elem *e, void *aux UNUSED) {
+  struct shared_table_entry *st =
+    hash_entry(e, struct shared_table_entry, elem);
+
+  return (unsigned) st->file;
+}
+
+/* Compares two shared table elems based on file pointer address */
+static bool cmp_file(const struct hash_elem *a,
+			     const struct hash_elem *b, void *aux UNUSED) {
+  struct shared_table_entry *st1 =
+    hash_entry(a, struct shared_table_entry, elem);
+  struct shared_table_entry *st2 =
+    hash_entry(b, struct shared_table_entry, elem);
+
+  return st1->file < st2->file;
+}
+
+/* Inserts hash_elem elem into the frame_table 
+   SHOULD BE CALLED WITH THE FRAME TABLE LOCK ACQUIRED */
 void ft_insert_entry(struct hash_elem *elem) {
   hash_insert(&frame_table, elem);
+}
+
+/* Inserts hash_elem elem into the shared_table 
+   SHOULD BE CALLED WITH THE SHARED TABLE LOCK ACQUIRED */
+void st_insert_entry(struct hash_elem *elem) {
+  hash_insert(&shared_table, elem);
 }
 
 /* Gets a frame table entry from its hash table with a matching user address
@@ -59,6 +99,24 @@ struct frame_table_entry *ft_find_entry(const void *uaddr) {
   return hash_entry(elem, struct frame_table_entry, elem);
 }
 
+/* Gets a shared table entry from its hash table with a matching file address
+   Takes in a file address to search for 
+   Returns pointer to the table entry or NULL if unsuccessful 
+   SHOULD BE CALLED WITH THE SHARED TABLE LOCK ACQUIRED */
+struct shared_table_entry *st_find_entry(const struct file *file) {
+  struct shared_table_entry key;
+  
+  key.file = file;
+
+  struct hash_elem *elem = hash_find(&shared_table, &key.elem);
+
+  if(elem == NULL) {
+    return NULL;
+  }
+
+  return hash_entry(elem, struct shared_table_entry, elem);
+}
+
 /* Removes a frame table entry from the table and frees it 
    Takes in the user virtual address of the entry to remove 
    Does nothing if the entry doesn't exist 
@@ -72,6 +130,19 @@ void ft_remove_entry(void *uaddr) {
   }
 }
 
+/* Removes a shared table entry from the table and frees it 
+   Takes in the file address of the entry to remove 
+   Does nothing if the entry doesn't exist 
+   SHOULD BE CALLED WITH THE SHARED TABLE LOCK ACQUIRED */
+void st_remove_entry(struct file *file) {
+  struct shared_table_entry *st = st_find_entry(file);
+
+  if(st != NULL) {
+    hash_delete(&shared_table, &st->elem);
+    free(st);
+  }
+}
+
 /* Functions for accessing frame_table_lock */
 void ft_lock_acquire(void) {
   lock_acquire(&frame_table_lock);
@@ -79,4 +150,13 @@ void ft_lock_acquire(void) {
 
 void ft_lock_release(void) {
   lock_release(&frame_table_lock);
+}
+
+/* Functions for accessing shared_table_lock */
+void st_lock_acquire(void) {
+  lock_acquire(&shared_table_lock);
+}
+
+void st_lock_release(void) {
+  lock_release(&shared_table_lock);
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -3,16 +3,28 @@
 
 #include <hash.h>
 #include "threads/synch.h"
+#include "filesys/file.h"
 
 /* TODO Note: The frame_table is now static, I hope this is alright. */
 /* Frame table */
 //extern struct hash frame_table;
 
+struct owners_list_elem {
+  struct thread *owner;         /* thread which owns this frame */
+  struct list_elem frame_elem;  /* Used to insert into frame's owners list */
+};
+
+struct shared_table_entry {
+  struct frame_table_entry *ft;
+  struct file *file;
+  struct hash_elem *elem;
+};
+
 /* Single row of the frame table */
 struct frame_table_entry {
   void *uaddr;           /* user virtual address frame represents */
   void *frame;           /* frame of memory that the data corresponds to */
-  struct thread *owner;  /* thread which the page belongs to */
+  struct list *owners;   /* threads which the page belongs to */
   int64_t timestamp;     /* time the frame was allocated in ticks */
   struct hash_elem elem; /* used to insert into the table */
   bool reference_bit;    /* used for second chance algorithm calculations */
@@ -28,8 +40,19 @@ void ft_insert_entry(struct hash_elem *elem);
 struct frame_table_entry *ft_find_entry(const void *uaddr);
 void ft_remove_entry(void *uaddr);
 
-/* Access functions for frame_table_lock */
+
+/* Initialise shared_table */
+void st_init(void);
+
+/* Manipulation of shared table */
+void st_insert_entry(struct hash_elem *elem);
+struct shared_table_entry *st_find_entry(const struct file *file);
+void st_remove_entry(struct file *file);
+
+/* Access functions for locks */
 void ft_lock_acquire(void);
 void ft_lock_release(void);
+void st_lock_acquire(void);
+void st_lock_release(void);
 
 #endif
